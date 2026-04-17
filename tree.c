f75eb35c8cd985b28b3649b65aa53a80f80d7ded
Bhuvi Dange <bhuvi.a.dange16@gmail.com>
2026-04-17 16:03:45 +0000

    phase2:tree: add base structure for tree_from_index and helper functions

--- tree.c
+++ tree.c
@@ -16,6 +16,11 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
+
+#include "index.h"
+#include "pes.h"
+
+int object_write(int type, const void *data, size_t len, ObjectID *id_out);
 // ─── Mode Constants ─────────────────────────────────────────────────────────
 
 #define MODE_FILE      0100644
