b5f2848c956b9b389ab94801201669c2e2a604dc
Bhuvi Dange <bhuvi.a.dange16@gmail.com>
2026-04-17 16:39:12 +0000

    tree: initialize tree_from_index and load index

--- tree.c
+++ tree.c
@@ -137,6 +137,19 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 int tree_from_index(ObjectID *id_out) {
     // TODO: Implement recursive tree building
     // (See Lab Appendix for logical steps)
+    int tree_from_index(ObjectID *id_out) {
+    Index index;
+
+    if (index_load(&index) != 0)
+        return -1;
+
+    Tree root;
+    root.count = 0;
+
+    (void)id_out;
+    return 0;
+}
+
     (void)id_out;
     return -1;
 }
